0a27346e51e0136e9483fad726f0b42a886231b8
dipesh <dipesh@192.168.10.119>
2017-05-25 22:31:48 -0400

    Euler 2.

--- src/euler/Fibonacci.java
+++ src/euler/Fibonacci.java
@@ -1,7 +1,9 @@
-package tests;
+package euler;
 
 /**
- * Example to print Nth Fibonacci odd, even, all numbers 
+ * Euler problem 2
+ * Example to print Nth Fibonacci odd, even, all numbers
+ * Add all even fib numbers up to seq less than 4000000 
  * @author Dipesh
  */
 public class Fibonacci {
@@ -15,25 +17,30 @@ public class Fibonacci {
 		ODD, EVEN, ALL;
 	}
 
+	public static long t = 0 ;
 	public static void fib(long i, long max, long p, long pp, Num n) {
 
 		// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 		if (i < max) {
 
 			long r = (i == 1) ? 1 : p + pp;
-
+			
 			switch (n) {
 			
 			case ALL:
 				// print all numbers ;
 				System.out.print(" " + r);
 				fib(++i, max, r, p, Num.ALL);
+				break ;
 
 			case EVEN:
-				if (r % 2 == 0)
-					System.out.println("N:  " + i + ", Val:  " + r);
+				if (r % 2 == 0 && r < 4000000) {
+					t = t + r ;
+					System.out.println("N:  " + i + ", Val:  " + r + ", Total:  " + t);
+				}
 
 				fib(++i, max, r, p, Num.EVEN);
+				break ;
 
 			case ODD:
 				if (r % 2 == 1)
